c6f18cd24b34e3523a5e0053cbe836bd288f735b
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2024-02-09 01:56:05 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Djroton Noé SOSSOU on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
